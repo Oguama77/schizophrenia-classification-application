e2a21ed9c935376da58797198f65537d24bcdce8
Oguama <oguamaprosperity@gmail.com>
2025-03-09 02:58:12 +0200

    correct formatting error

--- app.py
+++ app.py
@@ -113,7 +113,8 @@ def main():
     
     st.markdown(
     "<h1 style='text-align: center;'>Schizophrenia Detection from 3D MRI Scans 🩻</h1>", 
-    unsafe_allow_html=True
+    unsafe_allow_html=True)
+    
     st.write("")
     st.write("Upload a 3D MRI scan in NIfTI format (.nii, .nii.gz, or .zip) to check for schizophrenia.")
 
